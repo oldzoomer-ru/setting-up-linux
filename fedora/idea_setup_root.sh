d71e3ba1ff34541f9bae2ed095e0eec0e98fb9f6
oldzoomer-gh <gavrilovegor519-2@yandex.ru>
2025-04-18 12:32:58 +0300

    Fix idea_setup_root.sh

--- fedora/idea_setup_root.sh
+++ fedora/idea_setup_root.sh
@@ -5,6 +5,6 @@ if [ "$(id -u)" -ne 0 ]; then
         exit 1
 fi
 
-sudo dnf install java-21-openjdk-devel -y
+dnf install java-21-openjdk-devel -y
 mkdir /opt/idea-community-edition
 curl -L "https://download.jetbrains.com/product?code=IC&latest&distribution=linux" | tar xvz -C /opt/idea-community-edition --strip 1
