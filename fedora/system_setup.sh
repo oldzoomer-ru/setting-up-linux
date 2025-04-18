f20af448115aaa63e0a12bfa89b4f0f490af37ef
oldzoomer-gh <gavrilovegor519-2@yandex.ru>
2025-04-18 12:35:16 +0300

    Remove legacy moment from Fedora setting script

--- fedora/system_setup.sh
+++ fedora/system_setup.sh
@@ -5,9 +5,6 @@ if [ "$(id -u)" -ne 0 ]; then
         exit 1
 fi
 
-# Для AMDGPU
-grubby --update-kernel=ALL --args=rd.driver.pre=amdgpu
-
 # Для Realtek RTL8821CE
 echo "options rtw88_core disable_lps_deep=y" >> /etc/modprobe.d/rtw88.conf
 
