7c31af9d0a7315359d9ce90d71aec0c7439a13d5
oldzoomer-gh <gavrilovegor519-2@yandex.ru>
2025-05-17 22:57:18 +0300

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -26,10 +26,16 @@
     plymouth.enable = true;
 
     # Enable "Silent Boot"
+    consoleLogLevel = 0;
+    initrd.verbose = false;
     kernelParams = [
       "quiet"
       "splash"
       "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
     ];
   };
 
