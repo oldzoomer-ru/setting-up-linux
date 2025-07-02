684904b882248310492208bc76e90a396356674b
oldzoomer-ru <gavrilovegor519-2@yandex.ru>
2025-07-03 00:58:58 +0300

    Create configuration-amd-e1.nix

---
+++ config/configuration-amd-e1.nix
@@ -0,0 +1,103 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [ ./hardware-configuration.nix ];
+
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    tmp.useTmpfs = true;
+    extraModprobeConfig = ''options rtw88_core disable_lps_deep=y'';
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "loglevel=3"
+      "nowatchdog"
+      "zswap.enabled=1"
+      "zswap.compressor=lz4"
+      "zswap.max_pool_percent=20"
+    ];
+  };
+
+  services.journald.extraConfig = "SystemMaxUse=50M";
+
+  zramSwap.enable = true;
+  boot.kernel.sysctl = {
+    "vm.swappiness" = 30;
+    "vm.vfs_cache_pressure" = 500;
+  };
+
+  networking.hostName = "oldzoomer-laptop";
+  networking.networkmanager.enable = true;
+  time.timeZone = "Europe/Moscow";
+
+  i18n.defaultLocale = "ru_RU.UTF-8";
+  console = {
+    font = "cyr-sun16";
+    useXkbConfig = true;
+  };
+
+  services.xserver = {
+    enable = true;
+    desktopManager.lxde.enable = true;
+    displayManager = {
+      lightdm.enable = true;
+      autoLogin = {
+        enable = true;
+        user = "oldzoomer";
+      };
+    };
+    windowManager.openbox.enable = true;
+    xkb.layout = "us,ru";
+    xkb.options = "grp:caps_toggle";
+  };
+
+  environment.systemPackages = with pkgs; [
+    tree
+    less
+    efibootmgr
+    firefox
+    libreoffice-fresh
+    remmina
+    pcmanfm
+    xarchiver
+    qpdfview
+    mpv
+    htop
+    filelight
+    gnome-system-monitor
+  ];
+
+  services.tlp.enable = true;
+  services.thermald.enable = true;
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+    alsa.enable = true;
+  };
+
+  users.users.oldzoomer = {
+    isNormalUser = true;
+    extraGroups = ["wheel" "networkmanager"];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+  security.sudo.extraConfig = ''Defaults timestamp_timeout=30'';
+
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+    settings.auto-optimise-store = true;
+  };
+
+  system.stateVersion = "24.11";
+}
