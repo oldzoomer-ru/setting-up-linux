25633fa8e6ead8f78fb7112abefa08241fa7130e
oldzoomer-ru <gavrilovegor519-2@yandex.ru>
2025-07-03 01:23:36 +0300

    Update configuration.nix

--- config/configuration.nix
+++ config/configuration.nix
@@ -1,187 +1,133 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-{ config, lib, pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
-
-  boot = {
-    loader = {
-      systemd-boot.enable = true;
-      efi.canTouchEfiVariables = true;
-      timeout = 3;
-    };
-
-    tmp.useTmpfs = true;
-
-    extraModprobeConfig = ''
-      options rtw88_core disable_lps_deep=y
-    '';
-
-    plymouth.enable = true;
-
-    # Enable "Silent Boot"
-    consoleLogLevel = 0;
-    initrd.verbose = false;
-    kernelParams = [
-      "quiet"
-      "splash"
-      "boot.shell_on_fail"
-      "loglevel=3"
-      "rd.systemd.show_status=false"
-      "rd.udev.log_level=3"
-      "udev.log_priority=3"
-    ];
-  };
+  imports = [ ./hardware-configuration.nix ];
 
   system.autoUpgrade.enable = true;
+  system.copySystemConfiguration = true;
+  system.stateVersion = "24.11";
 
-  services.journald.extraConfig = "SystemMaxUse=50M";
-
-  networking.hostName = "egor-laptop"; # Define your hostname.
-
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-
-  # Set your time zone.
-  time.timeZone = "Europe/Moscow";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "ru_RU.UTF-8";
-  console = {
-    font = "cyr-sun16";
-    useXkbConfig = true; # use xkb.options in tty.
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+    timeout = 2;
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-
-  environment.gnome.excludePackages = with pkgs; [
-    file-roller
-    snapshot
-    gnome-maps
-    gnome-contacts
-    geary
-    simple-scan
-    gnome-calendar
-    gnome-font-viewer
-    gnome-characters
-    gnome-connections
-    gnome-tour
-    yelp
-    gnome-clocks
-    epiphany
+  boot.kernelParams = [
+    "quiet"
+    "splash"
+    "loglevel=3"
+    "rd.systemd.show_status=false"
+    "rd.udev.log_level=3"
+    "udev.log_priority=3"
   ];
 
-  # Configure keymap in X11
-  services.xserver.xkb.layout = "us,ru";
-  services.xserver.xkb.options = "grp:caps_toggle";
+  boot.consoleLogLevel = 0;
+  boot.initrd.verbose = false;
+  boot.plymouth.enable = true;
+  boot.tmp.useTmpfs = true;
+  boot.extraModprobeConfig = "options rtw88_core disable_lps_deep=y";
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+  services.fstrim.enable = true;
 
-  # Enable sound.
-  # hardware.pulseaudio.enable = true;
-  # OR
+  networking.hostName = "oldzoomer-laptop";
+  networking.networkmanager.enable = true;
+  networking.firewall = {
+    allowedTCPPortRanges = [{ from = 1714; to = 1764; }];
+    allowedUDPPortRanges = [{ from = 1714; to = 1764; }];
+    allowPing = false;
+  };
+
+  zramSwap.enable = true;
+
+  nix.gc.automatic = true;
+  nix.settings.auto-optimise-store = true;
+
+  services.earlyoom.enable = true;
+  services.journald.extraConfig = "SystemMaxUse=50M";
+
+  services.xserver = {
+    enable = true;
+    desktopManager.gnome.enable = true;
+    displayManager.gdm.enable = true;
+    xkb = {
+      layout = "us,ru";
+      options = "grp:caps_toggle";
+    };
+  };
+
+  services.tlp.enable = true;
   services.pipewire = {
     enable = true;
     pulse.enable = true;
+    alsa.enable = true;
+  };
+  services.flatpak.enable = true;
+  services.printing.enable = true;
+
+  virtualisation.docker.enable = true;
+
+  time.timeZone = "Europe/Moscow";
+  i18n.defaultLocale = "ru_RU.UTF-8";
+  console = {
+    font = "cyr-sun16";
+    useXkbConfig = true;
   };
 
-  # Define a user account. Don't forget to set a password with passwd.
-  users.users.egor = {
+  users.users.oldzoomer = {
     isNormalUser = true;
-    description  = "Egor";
-    extraGroups = [ "wheel" "networkmanager" "podman" ]; # Enable sudo for the user.
-    subGidRanges = [
-      {
-        count = 65536;
-        startGid = 100000;
-      }
-    ];
-    subUidRanges = [
-      {
-        count = 65536;
-        startUid = 100000;
-      }
-    ];
+    extraGroups = [ "wheel" "networkmanager" "docker" ];
   };
+
+  environment.variables = {
+    EDITOR = "nano";
+    VISUAL = "nano";
+  };
+  
+  security.sudo.extraConfig = ''
+    Defaults editor=${pkgs.nano}/bin/nano
+  '';
 
   nixpkgs.config.allowUnfree = true;
-
-  # programs.firefox.enable = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    wget
-    less
-    tree
-    efibootmgr
-    distrobox
-    podman-compose
-    google-chrome
+    wget htop tree efibootmgr nano
+    docker-compose google-chrome
+    gnomeExtensions.gsconnect
+    gnomeExtensions.vitals
+    gnomeExtensions.appindicator
+    gnomeExtensions.bing-wallpaper-changer
   ];
 
-  services.flatpak.enable = true;
+  environment.gnome.excludePackages = with pkgs; [
+    gnome-maps gnome-contacts geary gnome-calendar
+    gnome-characters gnome-connections gnome-tour epiphany
+  ];
 
-  virtualisation.podman = {
-    enable = true;
-    dockerCompat = true;
+  services.gnome.gnome-browser-connector.enable = true;
+  programs.dconf.enable = true;
+
+  dconf.settings = {
+    "org/gnome/shell" = {
+      enabled-extensions = [
+        "gsconnect@andyholmes.github.io"
+        "Vitals@CoreCoding.com"
+        "appindicatorsupport@rgcjonas.gmail.com"
+        "BingWallpaper@ineffable-gmail.com"
+      ];
+    };
+    
+    "org/gnome/shell/extensions/bingwallpaper" = {
+      country = "ru";
+      change-interval = "hourly";
+    };
+    
+    "org/gnome/shell/extensions/vitals" = {
+      show-storage = true;
+      show-voltage = false;
+      show-network = true;
+      show-temperature = true;
+      show-memory = true;
+      show-processor = true;
+    };
   };
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "24.11"; # Did you read the comment?
-
 }
